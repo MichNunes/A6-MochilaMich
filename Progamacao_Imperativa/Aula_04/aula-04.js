77a92ba645082777e52c1ae8957cef811aef874c
MichNunes <michnunes0@gmail.com>
2021-10-26 19:19:32 -0300

    Exercicios Aula 04 PI

---
+++ Progamacao_Imperativa/Aula_04/aula-04.js
@@ -0,0 +1,17 @@
+console.log("Olá mundo!");
+
+var x = 7;
+const y = 6;
+let z = 2;
+
+// let soma = x + y + z;
+// let subtracao = x - y - z;
+// let divisao = y / z;
+// let multiplicacao = x * y;
+// console.log(soma, subtracao, divisao, multiplicacao)
+
+let nome = "Michel";
+let idade = "20";
+
+console.log(nome, idade);
+
